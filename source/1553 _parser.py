6fbffdc3a336fd48314b43ac807e1dde83b3c9bb
Easy_EL <idan3439@gmail.com>
2022-12-28 14:51:15 +0200

    28/12/2022

--- source/1553 _parser.py
+++ source/1553 _parser.py
@@ -1,4 +1,4 @@
-from Py106.MsgDecode1553 import Decode1553F1
+from Py106.MsgDecode1553 import *
 from Py106.time import Time
 import Py106.packet as packet
 import Py106.status as status
@@ -8,13 +8,94 @@ import sys
 
 
 class Message:
-    def __init__(self, pkt_hdr: object, msg: object, decoder_1553: object) -> None:
-        self._msg_1553 = \
-            {
-                'HEADER_MSG': Message.get_1553_header(pkt_hdr, msg),
+    # ------------------------------------------------------------------------------------------------------------------
+    class Header_ch10:
+        def __init__(self, pkt_hdr: packet.Header):
+            # self.msg_error = msg.p1553Hdr.contents.Field.BlockStatus.MsgError
+            self.adapter_id = pkt_hdr.ch_id
+            self.data_type = packet.DataType.MIL1553_FMT_1
+            # self.time_tag = msg.p1553Hdr.contents.Field.PktTime
+            self.serial = pkt_hdr.seq_num
+            self.data_len = pkt_hdr.data_len
+            self.flags = pkt_hdr.packet_flags
 
-                'BODY_MSG': body_1553_msg(msg, decoder_1553)
-            }
+        def to_dict(self) -> dict:
+            return \
+                {
+                    # 'MSG_STATUS': c.FIX_STATUS if not self.msg_error else c.NOT_FIX_STATUS,
+                    'ADAPTER_ID': format(self.adapter_id, "04x"),  # pass
+                    'DATA_TYPE': format(self.data_type, "04x"),  # pass
+                    # 'TIME_TAG': format(self.time_tag, "016x"),
+                    'SERIAL': format(self.serial, "08x"),
+                    'DATA_LENGTH': format(self.data_len, "08x"),
+                    'HEADER_FLAGS': format(self.flags, "02x")
+                }
+
+        # TODO: move TIME TAG and MSG STATUS to -> Header_1553
+
+    # ------------------------------------------------------------------------------------------------------------------
+    class Header_1553:
+        T_R = ("R", "T")
+
+        @property
+        def bus_id(self):
+            # BUS 'A'-> 0 ; BUS 'B' -> 1
+            return chr(ord('A') + self._recorde.p1553Hdr.contents.Field.BlockStatus.BusID)
+
+        @staticmethod
+        def decode_command_word(cmd: CmdWord_Fields):
+            return cmd.Field.RTAddr, cmd.Field.SubAddr
+
+        @staticmethod
+        def hex_values(sts: StatWord, cmd: CmdWord):
+            return format(cmd.contents.Value, "04x"), format(sts.contents.Value, "04x")
+
+        def __init__(self, record: CurrMsg_1553F1, decoder: Decode1553F1):
+            self._record: CurrMsg_1553F1 = record
+
+            # decoding first command word
+            # TODO: check typehint in decode_command_word
+            self.tr_1, self.sa_1 = self.decode_command_word(self._recorde.pCmdWord1.contents.Field)
+            self.word_count = decoder.word_cnt(self._recorde.pCmdWord1.contents.Value)
+
+            # checking transmit or receive mode
+            self.tr_or_rec = self.T_R[self._recorde.pCmdWord1.contents.Field.TR]
+
+            # getting hex value of command and status word
+            self.cmd1_value, self.status1_value = self.hex_values(self._recorde.pCmdWord1,
+                                                                  self._recorde.pStatWord1)
+
+            # assuming that it's a transmit / receive message
+            tr_2 = None
+            sa_2 = None
+            cmd_word_2 = None
+            status_word_2 = None
+
+            # checking for rt2rt communicate messages
+            if record.p1553Hdr.contents.Field.BlockStatus.RT2RT != 0:
+                tr_2 = record.pCmdWord2.contents.Field.RTAddr
+                sa_2 = record.pCmdWord2.contents.Field.SubAddr
+                cmd_word_2 = format(record.pCmdWord2.contents.Value, "04x")
+                status_word_2 = format(record.pStatWord2.contents.Value, "04x")
+                tr_or_rec = "RT -> RT"
+
+            '''# init data words array
+            self.data_words = []'''
+
+            if record.p1553Hdr.contents.Field.BlockStatus.MsgError == 0:
+                self.data_words = list(map(lambda x: format(x, "04x"), record.pData.contents))
+            else:
+                self.data_words = 'Msg Error'
+
+        def is_rt_to_rt(self):
+            return self._record.p1553Hdr.contents.Field.BlockStatus.RT2RT != 0
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self, pkt_hdr: packet.Header, msg: CurrMsg_1553F1, decoder_1553: Decode1553F1) -> None:
+        self._header_ch10 = self.Header_ch10().to_dict()
+        self._header_1553 = Message.get_1553_header(pkt_hdr, msg)
+        self._content_1553 = body_1553_msg(msg, decoder_1553)
 
     @staticmethod
     def get_1553_header(pkt_hdr: object, msg: object) -> dict:
@@ -38,11 +119,11 @@ class Message:
         """
         @param content: object. contains all the 1553 msg in packet
         @param decoder_1553: the tool to decode the info in packet
-        @return: None
+        @return: dict
         """
         bus_id = ord(65 + content.p1553Hdr.contents.Field.BlockStatus.BusID)
 
-        # getting info we need from command word
+        # decoding first command word
         tr_1, sa_1 = Message.decode_cmd(content.pCmdWord1.contents.Field)
         word_count = decoder_1553.word_cnt(content.pCmdWord1.contents.Value)
 
@@ -72,17 +153,16 @@ class Message:
         data_words = []
 
         if content.p1553Hdr.contents.Field.BlockStatus.MsgError == 0:
-            for iDataIdx in range(word_count):
-                data_words.append(format(content.pData.contents[iDataIdx], "04x"))
+            data_words = list(map(lambda x: format(x, "04x"), content.pData.contents))
         else:
-            data_words.append('Msg Error')
+            data_words = 'Msg Error'
 
         # TODO: add additional 1553 flags
 
     @staticmethod
     def get_dict(**kwargs: object) -> dict:
         """
-        @param args: json keys and values
+        @param kwargs: json keys and values
         @return: dict
         """
         return kwargs
@@ -98,6 +178,18 @@ class Message:
     @staticmethod
     def decode_cmd(contents):
         return contents.Field.RTAddr, contents.Field.SubAddr
+
+    @staticmethod
+    def to_word_format(data):
+        format(data, "04x")
+
+    def to_dict(self) -> dict:
+        return
+        {
+            'HEADER_MSG': self._header,
+
+            'BODY_MSG': self._header
+        }
 
 
 def body_msg_dict(args) -> dict:
